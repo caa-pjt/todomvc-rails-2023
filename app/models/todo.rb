28b71f96faea47c1a1247a58ba785a42070bd9bc
Carlos Antunes <carlos.antunes@ik.me>
2023-11-23 20:46:43 +0100

    Solved - Ensure title presence and strip it

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ApplicationRecord
-  # TODO 1
-  # TODO 2
+  # TODO 1 tester la présence du todo (nom vide)
+  validates :title, presence: true
+  # TODO 2 supprime les espaces avant et aprés le titre (trim)
+  normalizes :title, with: ->(title) {title.strip}
 end
