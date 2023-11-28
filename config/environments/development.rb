97bf1a20573a5e103134927056a9c259cf655dcb
Carlos Antunes <carlos.antunes@ik.me>
2023-11-28 18:06:13 +0100

    Show application logs in a log file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,4 +72,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  Rails.logger = Logger.new(STDOUT)
+  config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
 end
